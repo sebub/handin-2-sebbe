ec99c977e9acddda4a079da6a90ff3f1fb04eb03
sebub <105357150+sebub@users.noreply.github.com>
2022-09-18 21:29:08 +0200

    Update handin2.c

--- src/handin2.c
+++ src/handin2.c
@@ -1,5 +1,36 @@
+#include <stdio.h>
 
 int main(void)
 {
+    int i,j, smallest,c,check;
+
+    while (1)
+    {
+        check = 0;
+        printf("Please enter 2 integers:\n");
+        scanf("%d %d", &i, &j);
+
+        if((i < 1) || (j < 1))
+            break;
+
+        printf("You have entered the numbers %d and %d\n", i, j);
+
+        smallest = i <= j ? i : j;
+
+        for(c = smallest; check < 1; c--)
+        {
+            int cj = j%c;
+            int ci = i%c;
+            if((ci == 0) && (cj == 0))
+            {
+                printf("Greatest common divisor of %d and %d is %d",i, j, c);
+                check = 1;
+            }
+        }
+
+
+
+    }
+
 
 }
